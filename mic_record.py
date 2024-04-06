a473d791ce791c4fa9daf59dd926b9ffc1254df2
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-04-05 20:26:55 -0500

    Update mic_record.py

--- mic_record.py
+++ mic_record.py
@@ -4,6 +4,8 @@ import pickle
 
 DEVICE = '/dev/ttyUSB0'
 
+TTY = serial.Serial(DEVICE, 9600)
+
 
 def pickle_save(data, file):
     with open(file, 'wb') as f:  # open a text file
@@ -11,49 +13,49 @@ def pickle_save(data, file):
         f.close()
 
 
-def read_serial(tty: serial.Serial):
+def read_serial():
     data = b''
-    while tty.inWaiting() == 0:
+    while TTY.inWaiting() == 0:
         # sleep(.5)
         pass
-    while tty.inWaiting() > 0:
-        bytesToRead = tty.inWaiting()
-        data += ser.read(bytesToRead)
+    while TTY.inWaiting() > 0:
+        data += TTY.read(1)
     print(data)
     return data
 
 
 if __name__ == "__main__":
     mic_data = {}
-    ser = serial.Serial(DEVICE, 9600)
+    TTY.isOpen()
     print('Press the buttons as they appear to record serial data')
 
     for i in range(0, 10):
         print(f'{str(i)}> ', end='')
-        mic_data[str(i)] = read_serial(ser)
+        mic_data[str(i)] = read_serial()
         sleep(1)
 
     print('*> ', end='')
-    mic_data['Star'] = read_serial(ser)
+    mic_data['Star'] = read_serial()
     sleep(1)
     print('#> ', end='')
-    mic_data['Pound'] = read_serial(ser)
+    mic_data['Pound'] = read_serial()
     sleep(1)
     print('AB> ', end='')
-    mic_data['AB'] = read_serial(ser)
+    mic_data['AB'] = read_serial()
     sleep(1)
 
     for i in range(1, 5):
         print(f'User{i}> ', end='')
-        mic_data[f'User{i}'] = read_serial(ser)
+        mic_data[f'User{i}'] = read_serial()
         sleep(1)
 
     print('TX(Hold Down)> ', end='')
-    mic_data['TXdown'] = read_serial(ser)
+    mic_data['TXdown'] = read_serial()
     sleep(1)
     print('TX(Release)> ', end='')
-    mic_data['TXup'] = read_serial(ser)
+    mic_data['TXup'] = read_serial()
     sleep(1)
 
     pickle_save(mic_data, "mic.dat")
     print('Mic recording complete.')
+    TTY.close()
