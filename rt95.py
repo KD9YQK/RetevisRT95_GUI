0b3e4b0363006c0507a722dcf57f9b5ba1cabec4
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2024-05-14 18:39:07 -0500

    Update rt95.py

--- rt95.py
+++ rt95.py
@@ -3,111 +3,112 @@ import serial
 from time import sleep
 import struct
 
-KEYS = {'RESET': b'AL~KR\r\n', 
-       '0': b'AL~K0\r\n', 
-       '1': b'AL~K1\r\n', 
-       '2': b'AL~K2\r\n', 
-       '3': b'AL~K3\r\n', 
-       '4': b'AL~K4\r\n', 
-       '5': b'AL~K5\r\n', 
-       '6': b'AL~K6\r\n', 
-       '7': b'AL~K7\r\n', 
-       '8': b'AL~K8\r\n', 
-       '9': b'AL~K9\r\n', 
-       '*': b'AL~K*\r\n', 
-       '#': b'AL~K#\r\n', 
-       'AB': b'AL~K/\r\n', 
-       'User1': b'AL~KA\r\n', 
-       'User2': b'AL~KB\r\n', 
-       'User3': b'AL~KC\r\n', 
-       'User4': b'AL~KD\r\n'}
+KEYS = {'RESET': b'AL~KR\r\n',
+        '0': b'AL~K0\r\n',
+        '1': b'AL~K1\r\n',
+        '2': b'AL~K2\r\n',
+        '3': b'AL~K3\r\n',
+        '4': b'AL~K4\r\n',
+        '5': b'AL~K5\r\n',
+        '6': b'AL~K6\r\n',
+        '7': b'AL~K7\r\n',
+        '8': b'AL~K8\r\n',
+        '9': b'AL~K9\r\n',
+        '*': b'AL~K*\r\n',
+        '#': b'AL~K#\r\n',
+        'AB': b'AL~K/\r\n',
+        'User1': b'AL~KA\r\n',
+        'User2': b'AL~KB\r\n',
+        'User3': b'AL~KC\r\n',
+        'User4': b'AL~KD\r\n'}
 
 KEY_LIST = []
 for k in KEYS:
-       KEY_LIST.append(KEYS[k])
+    KEY_LIST.append(KEYS[k])
+
 
 def to_struct(bytestring):
-       return struct.unpack(bytestring)
+    return struct.unpack('12b', bytestring)
 
 
 class RT95:
-       DEVICE = "/dev/ttyUSB0"
-       TTY: serial.Serial
+    DEVICE = "/dev/ttyUSB0"
+    TTY: serial.Serial
 
-       RX_A = False
-       RX_B = False
-       TX_A = False
-       TX_B = False
-       VFO = 'A'
+    RX_A = False
+    RX_B = False
+    TX_A = False
+    TX_B = False
+    VFO = 'A'
 
-       tx_buffer = []
+    tx_buffer = []
+    isRTS = False
 
-       def parse_data(data):
-              d_spl = data.split(b'ST')
-              d_spl.pop(0)
-              for d in d_spl:
-                     d_st = to_struct(d)
-                     if d_st[6] == 0:
-                            self.VFO = 'A'
-                     else:
-                            self.VFO = 'B'
-                     if d_st[4] == 7:
-                            self.TX_A = True
-                     else:
-                            self.TX_A = False
-                     if d_st[5] == 7:
-                            self.TX_B = True
-                     else:
-                            self.TX_B = False
-                     if d_st[10] == 1 and d_st[2] == 1:
-                            self.RX_A = True
-                     elif d_st[10] == 1 and d_st[2] == 0:
-                            self.RX_B = True
-                     else:
-                            self.RX_A = False
-                            self.RX_B = False
+    def parse_data(self, data):
+        d_spl = data.split(b'ST')
+        d_spl.pop(0)
+        for d in d_spl:
+            d_st = to_struct(d)
+            if d_st[6] == 0:
+                self.VFO = 'A'
+            else:
+                self.VFO = 'B'
+            if d_st[4] == 7:
+                self.TX_A = True
+            else:
+                self.TX_A = False
+            if d_st[5] == 7:
+                self.TX_B = True
+            else:
+                self.TX_B = False
+            if d_st[10] == 1 and d_st[2] == 1:
+                self.RX_A = True
+            elif d_st[10] == 1 and d_st[2] == 0:
+                self.RX_B = True
+            else:
+                self.RX_A = False
+                self.RX_B = False
 
+    def __init__(self, device="/dev/ttyUSB0", baud=9600):
+        self.DEVICE = device
+        self.TTY = serial.Serial(self.DEVICE, baud)
 
-       def __init__(self, device="/dev/ttyUSB0", baud=9600):
-              self.DEVICE = device
-              self.TTY = serial.Serial(self.DEVICE, baud)
+    def send_single(self, char):
+        self.tx_buffer.append(KEYS[char])
 
-       def send_single(self, char):
-              self.tx_buffer.append(KEYS[char])
+    def send_multiple(self, presses):
+        for char in presses:
+            self.tx_buffer.append(KEYS[char])
 
-       def send_multiple(self, presses):
-              for char in presses:
-                     self.tx_buffer.append(KEYS[char])
-              
-       async def main_loop(self):
-              data = b''
-              while self.TTY.inWaiting() == 0:
-                     data = b''
-                     while len(self.tx_buffer) > 0:
-                             self.TTY.write(self.tx_buffer[0])
-                             self.tx_buffer.pop(0)
-                             self.TTY.flush()
-                             await asyncio.sleep(.2)
-                     await asyncio.sleep(.2)
-              while self.TTY.inWaiting() > 0:
-                     data += self.TTY.read(1)
-                     if data in KEY_LIST:
-                            print(f'ECHO - ', end='')
-                            print(data)
-                            data = b''
-              if data != b'':
-                     self.parse_data(data)
-                     
+    async def main_loop(self):
+        data = b''
+        while self.TTY.inWaiting() == 0:
+            data = b''
+            while len(self.tx_buffer) > 0:
+                self.TTY.write(self.tx_buffer[0])
+                self.tx_buffer.pop(0)
+                self.TTY.flush()
+                await asyncio.sleep(.2)
+            await asyncio.sleep(.2)
+        while self.TTY.inWaiting() > 0:
+            data += self.TTY.read(1)
+            if data in KEY_LIST:
+                print(f'ECHO - ', end='')
+                print(data)
+                data = b''
+        if data != b'':
+            self.parse_data(data)
 
-       def setRTS(self, enable=True):
-              self.TTY.setRTS(enable)
+    def setRTS(self, enable=True):
+        self.isRTS = enable
+        self.TTY.setRTS(enable)
 
-       def setPTT(self, enable=True):
-              self.setRTS(enable)
+    def setPTT(self, enable=True):
+        self.setRTS(enable)
 
 
 if __name__ == "__main__":
-    radio = rt95()
+    radio = RT95()
     while True:
         radio.setRTS(True)
         sleep(1)
